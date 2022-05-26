9658befa77be734c0f7c3e4db07a36fee93d6622
Amresh <amresh2000@gmail.com>
2022-05-26 17:15:46 +0800

    Firebase Login Auth

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:verum_flutter/resources/auth_methods.dart';
 import 'package:verum_flutter/utils/colors.dart';
+import 'package:verum_flutter/utils/utils.dart';
 import 'package:verum_flutter/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,12 +15,28 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+  }
+
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res = await AuthMethods().loginUser(
+        email: _emailController.text, password: _passwordController.text);
+    if (res == "success") {
+    } else {
+      showSnackBar(res, context);
+    }
+    setState(() {
+      _isLoading = true;
+    });
   }
 
   @override
@@ -56,9 +74,15 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(height: 24),
               // button login
               InkWell(
-                onTap: () {},
+                onTap: loginUser,
                 child: Container(
-                  child: const Text('Log in'),
+                  child: _isLoading
+                      ? const Center(
+                          child: CircularProgressIndicator(
+                            color: primaryColor,
+                          ),
+                        )
+                      : const Text('Log in'),
                   width: double.infinity,
                   alignment: Alignment.center,
                   padding: const EdgeInsets.symmetric(vertical: 12),
