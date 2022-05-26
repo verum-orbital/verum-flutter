c028e2e75efba701767d16381e01211d4a3e816e
Amresh <amresh2000@gmail.com>
2022-05-26 13:40:51 +0800

    Firebase Signup Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// Testing Pull Request
-void main() {
+import 'package:verum_flutter/responsive/mobile_screen_layout.dart';
+import 'package:verum_flutter/responsive/responsive_layout_screen.dart';
+import 'package:verum_flutter/responsive/web_screen_layout.dart';
+import 'package:verum_flutter/screens/login_screen.dart';
+import 'package:verum_flutter/screens/signup_screen.dart';
+import 'package:verum_flutter/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: 'AIzaSyCD_aGzAggHQ4yg0WQ0yJCT5rBbYHIeU3o',
+        appId: '1:132209873388:web:1c54e0abce3201b059b0c9',
+        messagingSenderId: '132209873388',
+        projectId: 'verum-4bd2b',
+        storageBucket: 'verum-4bd2b.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -13,7 +35,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Verum',
-      home: const Text('Flutter Demo Home Page'),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      // home: const ResponsiveLayout(
+      //     webScreenLayout: WebScreenLayout(),
+      //     mobileScreenLayout: MobileScreenLayout(),
+      //     ),
+      home: SignupScreen(),
     );
   }
 }
