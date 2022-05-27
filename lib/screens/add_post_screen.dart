4f11cbf831058aa53ff37974a1a443c142c82f3d
Brandon <brandon.thio@yahoo.com.sg>
2022-05-27 14:05:44 +0800

    Add select & display image functionality via image_picker

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:verum_flutter/utils/utils.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
@@ -10,8 +12,63 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _image;
+
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Create a Post'),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take a Photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  var image = await selectImage(ImageSource.camera);
+                  setState(() {
+                    _image = image;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Upload from Library'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  var image = await selectImage(ImageSource.gallery);
+                  setState(() {
+                    _image = image;
+                    print('User selected Image');
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                  padding: const EdgeInsets.all(20),
+                  child: const Text('Cancel'),
+                  onPressed: () => Navigator.of(context).pop())
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Add Post Screen");
+    return _image == null
+        ? Center(
+            child: IconButton(
+              icon: const Icon(Icons.upload),
+              onPressed: () => _selectImage(context),
+            ),
+          )
+        : SizedBox(
+            child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: MemoryImage(_image!),
+                    fit: BoxFit.fitWidth,
+                    alignment: FractionalOffset.topCenter)),
+          ));
   }
 }
