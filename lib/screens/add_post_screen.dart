a05dd821424fd61fb5defb28b08130eaac641a76
Brandon <brandon.thio@yahoo.com.sg>
2022-05-27 14:35:32 +0800

    Polish UI

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,11 +1,15 @@
 import 'dart:typed_data';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:verum_flutter/models/user.dart' as userModel;
+import 'package:verum_flutter/providers/user_provider.dart';
 import 'package:verum_flutter/resources/firestore_methods.dart';
+import 'package:verum_flutter/utils/colors.dart';
+import 'package:verum_flutter/utils/global_variables.dart';
 import 'package:verum_flutter/utils/utils.dart';
-
-import '../utils/colors.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
@@ -18,12 +22,6 @@ class _AddPostScreenState extends State<AddPostScreen> {
   final TextEditingController _captionController = TextEditingController();
   Uint8List? _image;
   bool _isUploading = false;
-
-  void clearImage() {
-    setState(() {
-      _image = null;
-    });
-  }
 
   _selectImage(BuildContext context) async {
     return showDialog(
@@ -84,8 +82,22 @@ class _AddPostScreenState extends State<AddPostScreen> {
     }
   }
 
+  void clearImage() {
+    setState(() {
+      _image = null;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _captionController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final userModel.User user = Provider.of<UserProvider>(context).getUser;
+
     return _image == null
         ? Center(
             child: IconButton(
@@ -95,47 +107,63 @@ class _AddPostScreenState extends State<AddPostScreen> {
           )
         : Scaffold(
             appBar: AppBar(
-                backgroundColor: mobileBackgroundColor,
-                leading: IconButton(
-                    icon: const Icon(Icons.arrow_back), onPressed: clearImage),
-                title: const Text('Create Post'),
-                centerTitle: false,
-                actions: [
-                  TextButton(
-                      onPressed: uploadImage,
-                      child: const Text('Post',
-                          style: TextStyle(
-                              color: Colors.blueAccent,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16)))
-                ]),
+              backgroundColor: mobileBackgroundColor,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: clearImage,
+              ),
+              title: const Text('Create Post'),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                    onPressed: uploadImage,
+                    child: const Text('Post',
+                        style: TextStyle(
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16)))
+              ],
+            ),
             body: Column(
               children: [
                 _isUploading
                     ? const LinearProgressIndicator()
                     : const Padding(padding: EdgeInsets.only(top: 0)),
-                const Divider(),
-                SizedBox(
-                  height: 45,
-                  width: 45,
-                  child: AspectRatio(
-                    aspectRatio: 487 / 451,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: MemoryImage(_image!),
-                              fit: BoxFit.fill,
-                              alignment: FractionalOffset.topCenter)),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(user.avatarURL),
                     ),
-                  ),
-                ),
-                TextField(
-                  controller: _captionController,
-                  decoration: const InputDecoration(
-                      hintText: 'Caption...', border: InputBorder.none),
-                  maxLines: 8,
-                ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.45,
+                      child: TextField(
+                        controller: _captionController,
+                        decoration: const InputDecoration(
+                            hintText: 'Caption...', border: InputBorder.none),
+                        maxLines: 8,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: MemoryImage(_image!),
+                                  fit: BoxFit.fill,
+                                  alignment: FractionalOffset.topCenter)),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ],
-            ));
+            ),
+          );
   }
 }
