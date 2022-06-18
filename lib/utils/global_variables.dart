b6a8e9a5e168358ea12507bbc725e25315b3e134
Brandon <brandon.thio@yahoo.com.sg>
2022-06-18 22:14:50 +0800

    Add reusable profile page

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,5 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:verum_flutter/models/user.dart';
+import 'package:verum_flutter/models/user.dart' as model;
 import 'package:verum_flutter/screens/add_post_screen.dart';
 import 'package:verum_flutter/screens/feed_screen.dart';
 import 'package:verum_flutter/screens/profile_screen.dart';
@@ -8,12 +9,12 @@ import 'package:verum_flutter/screens/search_screen.dart';
 const webScreenWidth = 600;
 const placeholderAvatarImageURL =
     'https://www.kindpng.com/picc/m/207-2074624_white-gray-circle-avatar-png-transparent-png.png';
-const placeholderUser = User(
+const placeholderUser = model.User(
     email: '', username: '', bio: '', avatarURL: placeholderAvatarImageURL);
-const homeScreenWidgets = [
+final homeScreenWidgets = [
   FeedScreen(),
   SearchScreen(),
   AddPostScreen(),
   Text('Notifications'),
-  ProfileScreen(),
+  ProfileScreen(uid: null),
 ];
